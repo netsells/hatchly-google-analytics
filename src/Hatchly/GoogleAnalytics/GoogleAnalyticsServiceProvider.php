021394c724adcbe2e5e66d554fa842dcd3d79137
Phill Quantrill <phill.quantrill@netsells.co.uk>
2017-02-08 15:40:46 +0000

    HA-179 Fixed was missing use statements

--- src/Hatchly/GoogleAnalytics/GoogleAnalyticsServiceProvider.php
+++ src/Hatchly/GoogleAnalytics/GoogleAnalyticsServiceProvider.php
@@ -5,7 +5,12 @@ namespace Hatchly\GoogleAnalytics;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Hatchly\Settings\SettingModule;
-use Hatchly\GoogleAnalytics\Extensions;
+use Hatchly\GoogleAnalytics\Extensions\AnalyticsSettingPage;
+use Hatchly\GoogleAnalytics\Extensions\OauthAuthenticatedSetting;
+use Hatchly\GoogleAnalytics\Extensions\OauthAuthorisationCodeSetting;
+use Hatchly\GoogleAnalytics\Extensions\OauthTokenSetting;
+use Hatchly\GoogleAnalytics\Extensions\AnalyticsProfileSetting;
+use Hatchly\GoogleAnalytics\Extensions\CacheDurationSetting;
 
 class GoogleAnalyticsServiceProvider extends ServiceProvider
 {
@@ -28,7 +33,7 @@ class GoogleAnalyticsServiceProvider extends ServiceProvider
             SettingModule::registerSettingExtension($app->make(OauthAuthenticatedSetting::class));
 
             if (setting('analytics.oauth-authenticated')) {
-                
+
                 SettingModule::registerSettingExtension($app->make(OauthAuthorisationCodeSetting::class));
                 SettingModule::registerSettingExtension($app->make(OauthTokenSetting::class));
                 SettingModule::registerSettingExtension($app->make(AnalyticsProfileSetting::class));
